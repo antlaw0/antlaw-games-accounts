9992ea4575547279cea9e5d323e9e7024769140b
antlaw0 <antlaw0@gmail.com>
2025-05-20 07:20:50 -0500

    fixed port issue

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
+import os
 from flask import Flask
 from flask_cors import CORS
 from models import db, migrate
 from auth import auth_bp
 from config import Config
+
+port = int(os.environ.get('PORT', 5000))  # Render sets PORT
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -14,4 +17,4 @@ migrate.init_app(app, db)
 app.register_blueprint(auth_bp, url_prefix="/auth")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port=port)
